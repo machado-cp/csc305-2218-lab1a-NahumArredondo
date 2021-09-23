d16acf340dfcc66296f08ffdbb0cb54e8d4d4cb2
NahumArredondo <71236836+NahumArredondo@users.noreply.github.com>
2021-09-23 12:01:24 -0700

    Update Driver.java

--- src/edu/calpoly/csc305/lab1a/Driver.java
+++ src/edu/calpoly/csc305/lab1a/Driver.java
@@ -2,21 +2,18 @@ package edu.calpoly.csc305.lab1a;
 
 class Driver {
   public static void main(String[] args) {
-    Example ex = new Example();
     int[] list = {1, 2, 3, 4, 21};
     int total = 0;
     int x = 0;
+    Example ex = new Example("name", list);
 
-    ex.driver("name", list);
     System.out.println(ex.sameName("name"));
 
     while (x != list.length) {
-      int num = ex.getNums()[x];
-      total += num;
+      total += ex.getNums()[x];
       x += 1;
     }
 
     System.out.println(total);
   }
 }
-
